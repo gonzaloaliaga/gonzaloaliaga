7818e2678a200a230d6229f531545c4d7543d13a
gonzaloaliaga <gonz.aliaga@duocuc.cl>
2025-09-03 10:43:22 -0400

    Header

---
+++ scripts/loginListeners.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.getElementById("buttonOpenLogin");
+
+  button.addEventListener("click", function() {
+    console.log("Iniciar sesión presionado")
+  });
+});

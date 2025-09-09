fb16c6a6d8bfc0848ff6e05c692b469ddd62af64
gonz-aliaga <gonz.aliaga@duocuc.cl>
2025-09-09 19:15:08 -0300

    Cambio de ids, más estilos, refs #, form terminado

--- scripts/loginListeners.js
+++ scripts/loginListeners.js
@@ -1,7 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const button = document.getElementById("buttonOpenLogin");
 
-  button.addEventListener("click", function() {
-    console.log("Iniciar sesión presionado")
+  const buttonOpenLogin = document.getElementById("buttonOpenLogin");
+  buttonOpenLogin.addEventListener("click", function() {
+    console.log("Abrir iniciar sesión presionado")
   });
+
+  const buttonFormLogin = document.getElementById("buttonFormLogin");
+  buttonFormLogin.addEventListener("click", function() {
+    console.log("Iniciar sesión en formulario presionado")
+  });
+
+  const openRegister = document.getElementById("openRegister");
+  openRegister.addEventListener("click", function() {
+    console.log("Abrir registrar presioando")
+  })
+
+  const openHome = document.getElementById("openHome");
+  openHome.addEventListener("click", function() {
+    console.log("Abrir home presionado")
+  })
+
+
 });

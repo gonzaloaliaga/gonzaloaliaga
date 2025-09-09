6ab2dae282062a658dc6740cf8bce7921dbe50ee
gonz-aliaga <gonz.aliaga@duocuc.cl>
2025-09-09 20:06:42 -0300

    Avances en form registro, mejora de css y # a butt

--- scripts/loginListeners.js
+++ scripts/loginListeners.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const formLogin = document.getElementById("formLogin");
+  const formRegister = document.getElementById("formRegister");
+
+  /* HEADER */
+  const openHome = document.getElementById("openHome");
+  openHome.addEventListener("click", function() {
+    console.log("Abrir home presionado")
+  })
 
   const buttonOpenLogin = document.getElementById("buttonOpenLogin");
   buttonOpenLogin.addEventListener("click", function() {
     console.log("Abrir iniciar sesión presionado")
   });
 
+  /* LOGIN FORM */
   const buttonFormLogin = document.getElementById("buttonFormLogin");
   buttonFormLogin.addEventListener("click", function() {
     console.log("Iniciar sesión en formulario presionado")
@@ -12,13 +21,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const openRegister = document.getElementById("openRegister");
   openRegister.addEventListener("click", function() {
-    console.log("Abrir registrar presioando")
+    console.log("Abrir registrar presionado")
   })
 
-  const openHome = document.getElementById("openHome");
-  openHome.addEventListener("click", function() {
-    console.log("Abrir home presionado")
+  /* REGISTER FORM */
+  const buttonRegisterPet = document.getElementById("buttonRegisterPet");
+  openRegister.addEventListener("click", function() {
+    console.log("Añadir nuevo registro presionado")
   })
-
 
 });

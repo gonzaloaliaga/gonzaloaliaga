1988e4fd1aaa0f7a0c52955be3043382895bf37d
gonzaloaliaga <gonz.aliaga@duocuc.cl>
2025-09-10 10:21:12 -0300

    nuevo proyecto

---
+++ homePage/scripts/homePageListeners.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  /*
+  const openHome = document.getElementById("openHome");
+  openHome.addEventListener("click", function() {
+    console.log("Abrir home presionado")
+  });
+  */
+
+});
